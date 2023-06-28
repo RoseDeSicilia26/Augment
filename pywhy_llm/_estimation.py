691504ccae0b5ee44e18346944ab9e726b82214d
Rose De Sicilia <72463845+RoseDeSicilia26@users.noreply.github.com>
2023-06-28 15:27:57 -0700

    added guidance to import list and method signatures

--- pywhy_llm/_estimation.py
+++ pywhy_llm/_estimation.py
@@ -1,8 +1,8 @@
-from typing import List, Dict, Set, Tuple, Protocol
+from typing import Protocol
+import guidance
 
 class Estimation_Protocol(Protocol):
-
-    def suggest_estimation_code(self) -> str:
+    def suggest_estimation_code(self, llm: guidance.llms) -> str:
         """
         Suggest code to run the causal effect analysis
 
---
+++ pywhy_llm/suggest_model.py
@@ -0,0 +1,4 @@
+from ._model import Model_Protocol
+
+class Suggest_Model(Model_Protocol):
+    
